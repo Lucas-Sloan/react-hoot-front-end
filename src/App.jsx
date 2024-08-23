f8fec3d9cc644f00dbaf7e9cfdd7cbfd0719a8be
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-23 13:17:10 -0500

    Finished the exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+// src/App.jsx
+
+import { useState, createContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
 import * as authService from '../src/services/authService'; // import the authservice
+import * as hootService from './services/hootService';
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+    navigate('/hoots');
+  };
+
+  const handleDeleteHoot = async (hootId) => {
+    // Call upon the service function:
+    const deletedHoot = await hootService.deleteHoot(hootId);
+    // Filter state using deletedHoot._id:
+    setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
+    // Redirect the user:
+    navigate('/hoots');
+  };
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+  
+      // Set state:
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
 
   const handleSignout = () => {
     authService.signout();
@@ -23,8 +56,18 @@ const App = () => {
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
           {user ? (
-            <Route path="/" element={<Dashboard user={user} />} />
+            // Protected Routes:
+            <>
+              <Route path="/" element={<Dashboard user={user} />} />
+              <Route path="/hoots" element={<HootList hoots={hoots} />} />
+              <Route
+                path="/hoots/:hootId"
+                element={<HootDetails handleDeleteHoot={handleDeleteHoot} />}
+              />
+              <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+            </>
           ) : (
+            // Public Route:
             <Route path="/" element={<Landing />} />
           )}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
@@ -36,3 +79,4 @@ const App = () => {
 };
 
 export default App;
+
